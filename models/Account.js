4c0b6b30e59fcfaf2be8d14d4ec118bafd1a868b
Andrew Hudgins <andrewhudgins15@gmail.com>
2022-08-24 14:45:56 -0500

    Created Account model

---
+++ models/Account.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// creates the Account model
+class Account extends Model {
+    // methods go here
+};
+
+// create fields (columns) for the model using .init for model definition
+Account.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true
+        }
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [8],
+            isAlphanumeric: true
+        }
+    }
+},
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'account'
+});
+
+model.exports = Account;
