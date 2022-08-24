7f90947043f80d51f244e4ac65a3c2dae95334ee
Andrew Hudgins <andrewhudgins15@gmail.com>
2022-08-24 14:30:22 -0500

    Set up the server -- doesn't accept app.use('./controllers')

---
+++ server.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+// create the express application
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+// const session = require('express-session');
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// app.use function adds a new middleware to the app; these are executed when a request hits the backend
+// express.json() parses incoming JSON requests and puts the parsed data in req.body
+app.use(express.json());
+// URL-encoded data will be parsed w/ qs library allowing creation of nested objects from query strings
+// and does not filter out ? from the query string
+app.use(express.urlencoded({ extended: true }));
+// serves static files upon 
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(require('./controllers/'));
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on localhost:${PORT}`));
+});
