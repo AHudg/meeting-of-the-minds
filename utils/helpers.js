0ffceacfac7f0d519bab749491db5a6d3edc6d17
Andrew Hudgins <andrewhudgins15@gmail.com>
2022-08-25 00:40:14 -0500

    Applied the helpers to the landingpage.handlebars

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,5 @@
 module.exports = {
     fixDate: date => {
-        const formattedDate = `${new Date(date).toLocaleDateString('en-us', { month:"long", day:"numeric", year:"numeric" })}`;
-        console.log(formattedDate);
-        return formattedDate;
+        return `${new Date(date).toLocaleDateString('en-us', { month:"long", day:"numeric", year:"numeric" })}`;
     }
 }
