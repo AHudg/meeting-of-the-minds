c7d25a128cc499d31d4b809b8eae753399b11696
Andrew Hudgins <andrewhudgins15@gmail.com>
2022-08-25 19:02:54 -0500

    Finished logout ability - the console outputs an error about reading null for .addEventListener but it works- same error when I tried a button rather than an <a>

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,10 +1,15 @@
-async function logout() {
+async function logoutButtonHandler(event) {
+    console.log(event);
     const response = await fetch('api/accounts/logout', {
         method: 'post',
         headers: { 'Content-Type': 'application/json' }
     });
 
     if (response.ok) {
-        document.location.replace('/');
+        document.location.replace('/login');
+    } else {
+        alert(response.statusText);
     }
 }
+
+document.querySelector('#logout-btn').addEventListener('click', logoutButtonHandler);
