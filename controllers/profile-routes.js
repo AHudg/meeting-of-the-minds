5d09718e4844082637922d35766d453cbc75b125
Andrew Hudgins <andrewhudgins15@gmail.com>
2022-08-27 09:19:25 -0500

    Added edit button and created route to /posts/edit

--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -31,4 +31,31 @@ router.get('/', authenticate, (req, res) => {
     });
 });
 
+router.get('/edit/:id', authenticate, (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id','title','content','created_at'],
+        include: {
+            model: Account,
+            attributes: ['username']
+        }
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No user found with this id.' });
+            return;
+        }
+
+        const posts = dbPostData.get({ plain: true });
+
+        res.render('edit-post', { posts, loggedIn: true});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
